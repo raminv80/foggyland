a9944cb9a55374e553327b4c2c3dc7f0de560851
Lim Victor <github.viclim@gmail.com>
2014-12-06 17:13:59 +0800

    enable pg hstore

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206081148) do
+ActiveRecord::Schema.define(version: 20141206091317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "air_quality_indices", force: true do |t|
     t.date     "observe_date"
