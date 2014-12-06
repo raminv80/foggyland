5b84f62a310327c97d4a06ceb7e25d972137bf28
Lim Victor <github.viclim@gmail.com>
2014-12-06 16:12:34 +0800

    generate db

---
+++ db/schema.rb
@@ -0,0 +1,69 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141206081148) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "air_quality_indices", force: true do |t|
+    t.date     "observe_date"
+    t.integer  "value"
+    t.integer  "zip_code_id"
+    t.integer  "rating_number"
+    t.string   "rating_name"
+    t.decimal  "latitude"
+    t.decimal  "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "air_quality_indices", ["observe_date"], name: "index_air_quality_indices_on_observe_date", using: :btree
+  add_index "air_quality_indices", ["zip_code_id"], name: "index_air_quality_indices_on_zip_code_id", using: :btree
+
+  create_table "cities", force: true do |t|
+    t.string   "name"
+    t.boolean  "capital"
+    t.integer  "state_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "cities", ["state_id"], name: "index_cities_on_state_id", using: :btree
+
+  create_table "countries", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "states", force: true do |t|
+    t.string   "name"
+    t.string   "code"
+    t.integer  "country_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "states", ["country_id"], name: "index_states_on_country_id", using: :btree
+
+  create_table "zip_codes", force: true do |t|
+    t.string   "code"
+    t.integer  "city_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "zip_codes", ["city_id"], name: "index_zip_codes_on_city_id", using: :btree
+
+end
