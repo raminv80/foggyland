e0f62c4f845f25a4a25c95865eab44f78eb62f2f
Lim Victor <github.viclim@gmail.com>
2014-12-06 19:42:20 +0800

    fix api client

--- app/models/api/airnow.rb
+++ app/models/api/airnow.rb
@@ -4,16 +4,40 @@ module API
     base_uri 'www.airnowapi.org/aq/observation/zipCode/historical'
     default_params format: 'application/json'
 
+    def initialize
+      self.class.default_params API_KEY: api_keys.sample
+    end
+
     def fetch(zip_code, date)
-      date_param = date.strftime('&Y-%m-%dT00-0000')
+      date_param = date.strftime('%Y-%m-%dT00-0000')
       options =
         {
-          zip_code: zip_code,
-          date: date_param,
-          API_KEY: api_keys.sample
+          'zipCode' => zip_code,
+          'date' => date_param
         }
-      results = self.class.get('/', options)
-      Hash[results.map { |r| [r['ParameterName', r]] }]
+      responses = self.class.get('/', query: options)
+      return false if responses.empty?
+      results = Hash[responses.map { |r| [r['ParameterName'], r] }]
+      response = responses.first
+      {
+        'date_observed' => response['DateObserved'].to_datetime,
+        'latitude' => response['Latitude'],
+        'longitude' => response['Longitude'],
+        'ozone' => format_parameter(results['OZONE']),
+        'pm2.5' => format_parameter(results['PM2.5']),
+        'pm10' => format_parameter(results['PM10'])
+      }
+    end
+
+    def format_parameter(parameter)
+      return nil unless parameter
+      {
+        'aqi' => parameter['AQI'],
+        'category' => {
+          'number' => parameter['Category']['Number'],
+          'name' => parameter['Category']['Name']
+        }
+      }
     end
 
     def api_keys
