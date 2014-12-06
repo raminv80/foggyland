b116d970b5c65aa5655fcf3384cec3a116202516
Lim Victor <github.viclim@gmail.com>
2014-12-06 16:20:22 +0800

    db seeding for data needed to pull air data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+capitals = [
+  'Trenton', 'Topeka', 'Tallahassee', 'Springfield', 'Santa Fe',
+  'Salt Lake City', 'Salem', 'Saint Paul', 'Sacramento', 'Richmond',
+  'Raleigh', 'Providence', 'Pierre', 'Phoenix', 'Olympia', 'Oklahoma City',
+  'Nashville', 'Montpelier', 'Montgomery', 'Madison', 'Little Rock', 'Lincoln',
+  'Lansing', 'Juneau', 'Jefferson City', 'Jackson', 'Indianapolis', 'Honolulu',
+  'Helena', 'Hartford', 'Harrisburg', 'Frankfort', 'Dover', 'Des Moines',
+  'Denver', 'Concord', 'Columbus', 'Columbia', 'Cheyenne', 'Charleston',
+  'Carson City', 'Boston', 'Boise', 'Bismarck', 'Baton Rouge', 'Austin',
+  'Augusta', 'Atlanta', 'Annapolis', 'Albany'
+]
+country = Country.create(name: 'United States')
+
+CSV.foreach("#{Rails.root}/db/zipcodes.csv", headers: false) do |row|
+  next if row.empty?
+
+  zip_code, city_name, state_code = row
+  state = country.states.find_or_create_by(code: state_code)
+  city = state.cities.find_or_create_by(name: city_name)
+  city.zip_codes.create(code: zip_code)
+end
+
+City.where(name: capitals).update_all(capital: true)
