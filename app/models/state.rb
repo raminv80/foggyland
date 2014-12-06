ff6f6b6e43675b62085029e29ae82ae3a1ae20f1
Lim Victor <github.viclim@gmail.com>
2014-12-06 17:48:39 +0800

    change state validation setting

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,5 +2,5 @@ class State < ActiveRecord::Base
   belongs_to :country
   has_many :cities
 
-  validates :name, presence: true, uniqueness: true
+  validates :code, presence: true, uniqueness: true
 end
